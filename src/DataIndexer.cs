669a5e6c4bda6dc60303c534f9ed3ec93cb6c8e0
kkasj <kkostowski@student.agh.edu.pl>
2024-02-11 20:21:33 +0100

    dataindexer

---
+++ src/DataIndexer.cs
@@ -0,0 +1,33 @@
+public class DataIndexer
+{
+    const int DATA_LENGTH = 5;
+
+    public static string FromIndex(int index)
+    {
+        // convert index to data string
+        string data = "";
+
+        for (int i = 0; i < DATA_LENGTH; i++)
+        {
+            data = (char)('a' + index % 26) + data;
+            index /= 26;
+        }
+
+        return data;
+    }
+
+    public static int ToIndex(string data)
+    {
+        // convert data string to index
+        // inverse of FromIndex
+        int index = 0;
+
+        for (int i = 0; i < DATA_LENGTH; i++)
+        {
+            index *= 26;
+            index += data[i] - 'a';
+        }
+
+        return index;
+    }
+}
